848a2f7aa83fd26b60b0be3b157e909bf2e0f803
fabioitj <53172317+fabioitj@users.noreply.github.com>
2022-11-13 02:01:13 -0300

    commit
    commit

--- cacc_frontend/src/hooks/https.js
+++ cacc_frontend/src/hooks/https.js
@@ -2,10 +2,6 @@ const url = "http://localhost:80/cacc_backend/api/";
 var config = {
     headers: {
         'Content-Type': 'application/json'
-    },
-    proxy: {
-        host: 'localhost',
-        port: 3000
     }
 };
 
